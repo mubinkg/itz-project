18968fd39562d5c841c345523e4f754a819651ed
mubin.khan <mubin.khan@upaybd.com>
2025-04-22 17:47:31 +0600

    add prisma and sqlite db

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,6 +1,10 @@
 import { LoginForm } from "@/components/login-form"
+import { prisma } from "@/lib/db"
 
-export default function Page() {
+export default async function Page() {
+  const users = await prisma.user.findMany()
+  console.log(users)
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">

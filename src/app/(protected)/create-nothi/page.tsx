53d5354fade5c8fa00578ceb1848329bd87bd5c6
Mubin Mohammad Ikhtiar Khan <129994262+mubinkg@users.noreply.github.com>
2025-05-23 20:20:08 +0600

    update data table

--- src/app/(protected)/create-nothi/page.tsx
+++ src/app/(protected)/create-nothi/page.tsx
@@ -1,4 +1,5 @@
 import CreateNothi from '@/components/nothi/CreateNothi';
+import NothiList from '@/components/nothi/NothiList';
 import { Button } from '@/components/ui/button';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { prisma } from '@/lib/db';
@@ -28,68 +29,7 @@ export default async function Page() {
         মৌজার নথি তৈরি করুন
       </h1>
       <CreateNothi mouzaData={mouzaData} />
-      <div className="rounded-md border overflow-x-auto w-[81.5vw]">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead rowSpan={2} className="border text-center">
-                ক্র. নং
-              </TableHead>
-              <TableHead rowSpan={2} className="border text-center">
-                নথি নম্বর
-              </TableHead>
-              <TableHead colSpan={5} className="border text-center">
-                তফসিল
-              </TableHead>
-              <TableHead rowSpan={2} className="border text-wrap text-center">
-                জমির সরবমোট গ্রীন বরাদ্দ/নবায়ন/ নবায়নের সাল
-              </TableHead>
-              <TableHead rowSpan={2} className="border text-center">
-                গ্রীন বরাদ্দাতার নাম ঠিকানা ও মোবাইল নম্বর/ অধিষ্ঠ প্রতিষ্ঠানের নাম ও মোবাইল নম্বর
-              </TableHead>
-              <TableHead rowSpan={2} className="border text-center">
-                মামলা সংক্রান্ত তথ্য (যদি থাকে)
-              </TableHead>
-              <TableHead rowSpan={2} className="border text-center">
-                মন্তব্য
-              </TableHead>
-            </TableRow>
-            <TableRow>
-              <TableHead className="border text-center">মৌজা</TableHead>
-              <TableHead className="border text-center">জে.এল. নং</TableHead>
-              <TableHead className="border text-center">খতিয়ান নং</TableHead>
-              <TableHead className="border text-center">দাগ নং</TableHead>
-              <TableHead className="border text-center">পরিমাণ</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {
-              nothiList.map((nothi, index) => (
-                <TableRow key={nothi.id}>
-                  <TableCell className="border text-center">{index + 1}</TableCell>
-                  <TableCell className="border text-center">{nothi.caseNo}</TableCell>
-                  <TableCell className="border text-center">{nothi.mouja.name}</TableCell>
-                  <TableCell className="border text-center">{nothi.mouja.jlNo}</TableCell>
-                  <TableCell className="border text-center">১১১</TableCell>
-                  <TableCell className="border text-center">৩৯৭</TableCell>
-                  <TableCell className="border text-center">০.০৭</TableCell>
-                  <TableCell className="border text-center">আম বাগান</TableCell>
-                  <TableCell className="border text-center">১৪২৯</TableCell>
-                  <TableCell className="border text-center">
-                    মোঃ সালেহ হোসেন পিতা- মোঃ মজিদ উদ্দিন সাং- জগৎড়িপাড়া, দিনাজপুর
-                  </TableCell>
-                  <TableCell className="border text-center"></TableCell>
-                </TableRow>
-              ))
-            }
-          </TableBody>
-        </Table>
-      </div>
-
-      {/* Bottom Save Button */}
-      <div className="mt-6 flex justify-end">
-        <Button className="bg-green-600 hover:bg-green-700">নথি সংরক্ষণ</Button>
-      </div>
+      <NothiList nothiList={nothiList} />
     </div>
   );
 }
--- src/components/nothi/NothiList.tsx
+++ src/components/nothi/NothiList.tsx
@@ -1,24 +1,77 @@
-'use client';
-
-import React, { useState } from 'react';
-import CreateOwner from './CreateOwner';
-import CreateSurvey from './CreateSurvey';
+import React from 'react'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
+import { Nothi } from '@/generated/prisma'
 
 const NothiList = ({ nothiList }: { nothiList: Record<string, any>[] }) => {
   return (
-    <tbody>
-      {nothiList.map(nothi => (
-        <tr key={nothi.id} className="border-b">
-          <td className="p-3 whitespace-nowrap">{nothi.caseNo}</td>
-          <td className="p-3 whitespace-nowrap">{nothi.banglaYear}</td>
-          <td className="p-3">
-            <CreateOwner nothiId={nothi.id} previousOwners={nothi.nothiOwner} />
-          </td>
-          <CreateSurvey nothiId={nothi.id} landSurvey={nothi.landSurvey} />
-        </tr>
-      ))}
-    </tbody>
-  );
-};
+    <div className="rounded-md border overflow-x-auto w-[81.5vw]">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead rowSpan={2} className="border text-center">
+              ক্র. নং
+            </TableHead>
+            <TableHead rowSpan={2} className="border text-center">
+              নথি নম্বর
+            </TableHead>
+            <TableHead colSpan={5} className="border text-center">
+              তফসিল
+            </TableHead>
+            <TableHead rowSpan={2} className="border text-wrap text-center">
+              জমির সরবমোট গ্রীন বরাদ্দ/নবায়ন/ নবায়নের সাল
+            </TableHead>
+            <TableHead rowSpan={2} className="border text-center">
+              গ্রীন বরাদ্দাতার নাম ঠিকানা ও মোবাইল নম্বর/ অধিষ্ঠ প্রতিষ্ঠানের নাম ও মোবাইল নম্বর
+            </TableHead>
+            <TableHead rowSpan={2} className="border text-center">
+              মামলা সংক্রান্ত তথ্য (যদি থাকে)
+            </TableHead>
+            <TableHead rowSpan={2} className="border text-center">
+              মন্তব্য
+            </TableHead>
+          </TableRow>
+          <TableRow>
+            <TableHead className="border text-center">মৌজা</TableHead>
+            <TableHead className="border text-center">জে.এল. নং</TableHead>
+            <TableHead className="border text-center">খতিয়ান নং</TableHead>
+            <TableHead className="border text-center">দাগ নং</TableHead>
+            <TableHead className="border text-center">পরিমাণ</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {
+            nothiList.map((nothi, index) => (
+              <TableRow key={nothi.id}>
+                <TableCell className="border text-center">{index + 1}</TableCell>
+                <TableCell className="border text-center">{nothi.caseNo}</TableCell>
+                <TableCell className="border text-center">{nothi.mouja.name}</TableCell>
+                <TableCell className="border text-center">{nothi.mouja.jlNo}</TableCell>
+                <TableCell className="border text-center">{nothi.khotianNo}</TableCell>
+                <TableCell className="border text-center">{nothi.lineNo}</TableCell>
+                <TableCell className="border text-center">{nothi.quantity}</TableCell>
+                <TableCell className="border text-center">{nothi.renewalDate}</TableCell>
+                <TableCell className="border text-center">
+                  {nothi.name}
+                  <br />
+                  পিতা- {nothi.parentName}
+                  <br />
+                  সাং- {nothi.address}
+                  <br />
+                  {nothi.mobile}
+                </TableCell>
+                <TableCell className="border text-center">
+                  {
+                    nothi.caseInfo
+                  }
+                </TableCell>
+                <TableCell className="border text-center">{nothi.comment}</TableCell>
+              </TableRow>
+            ))
+          }
+        </TableBody>
+      </Table>
+    </div>
+  )
+}
 
-export default NothiList;
+export default NothiList
